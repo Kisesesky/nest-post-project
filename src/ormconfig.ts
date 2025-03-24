7b79d1332652416a054c24746b61a07bc7692404
Hoya <kisesesky19@gmail.com>
2025-03-24 16:09:42 +0900

    feat

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -20,7 +20,9 @@ export const AppDataSource = new DataSource({
     username: dbconfigService.dbUser,
     password: dbconfigService.dbPassword,
     database: dbconfigService.dbName,
-    ssl: false,
+    ssl:{
+        rejectUnauthorized: false,
+      },
     synchronize: false, //Migrations = synchronize: false
     entities: [entity],
     migrations: [migration],
