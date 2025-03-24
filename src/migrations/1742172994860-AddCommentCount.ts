8c1408f96aa935bfcc85b7bd4c8066f3f0be44b5
Hoya <kisesesky19@gmail.com>
2025-03-24 16:23:33 +0900

    Update 1742172994860-AddCommentCount.ts

--- src/migrations/1742172994860-AddCommentCount.ts
+++ src/migrations/1742172994860-AddCommentCount.ts
@@ -4,11 +4,10 @@ export class AddCommentCount1742172994860 implements MigrationInterface {
     name = 'AddCommentCount1742172994860'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "post" RENAME COLUMN "comment" TO "commentCount"`);
+        await queryRunner.query(`ALTER TABLE "post" ADD COLUMN "commentCount" INTEGER DEFAULT 0`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "post" RENAME COLUMN "commentCount" TO "comment"`);
+        await queryRunner.query(`ALTER TABLE "post" DROP COLUMN "commentCount"`);
     }
-
 }
